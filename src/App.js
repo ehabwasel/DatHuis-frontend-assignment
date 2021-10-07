b000f8a07f3fccaca3a45202bda3511ec05098f2
ehabwasel <ehab.wasel@yahoo.com>
2021-10-07 17:35:06 +0200

    create component with styled folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+// Render a styled text input with the standard input color, and one with a custom input color
+
+const App = () => {
+  return (
+    <>
+      <h1>DatHuis frontend assignment</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/component/styled/appContainer.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  padding-top: 15vh;
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+`;
+export const StyledWrapper = styled.div`
+  position: relative;
+  width: 300px;
+  margin: 0 auto;
+  justify-content: center;
+`;

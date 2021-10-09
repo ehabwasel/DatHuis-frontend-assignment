8407d54b31ab55d7f5036516feec2f4466a71ea6
ehabwasel <ehab.wasel@yahoo.com>
2021-10-09 11:58:20 +0200

    add theme files

---
+++ src/theme/theme.style.js
@@ -0,0 +1,15 @@
+const Theme = {
+  colors: {
+    dark: '#798697',
+    extraDark: '#4a4a4a',
+    light: '#bfc5cd',
+    error:'red',
+  },
+  fontSizes: {
+    primary: '20px',
+    medium:'17px',
+    small: '0.9rem',
+  },
+};
+
+export default Theme;
